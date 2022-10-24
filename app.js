6a157661822af1253f8837dcf90e311012399e2d
Dianakariuki <dianakariuki842@gmail.com>
2022-10-24 09:26:54 +0300

    ft: function solve

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-function display (val){
+function display(val){
     document.getElementById("result").value+=val;
-    
+
+}
+function solv(){
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
 }
