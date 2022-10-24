a8b3126498fc52e93c268ad168cd56b360ba17c4
Dianakariuki <dianakariuki842@gmail.com>
2022-10-24 12:24:55 +0300

    updated readme

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function display(val){
     document.getElementById("result").value+=val;
 
 }
-function solve(){
+function solve  (){
     let x = document.getElementById("result").value;
     let y = eval(x);
     document.getElementById("result").value = y;
